e2b7e7e2fbfd8de95171e12199a03e7636489336
AYIKORU INGRID <ingridayikoru@gmail.com>
2025-09-09 07:20:31 +0000

    m

--- movie-xd/vite.config.js
+++ movie-xd/vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
  
-export default defineConfig({
-  plugins: [react()],
-});
+// vite.config.js
+export default {
+  server: {
+    hmr: {
+      host: 'bug-free-journey-wrxq6r5x994wcg466-5173.app.github.dev',
+      protocol: 'wss',
+    }
+  }
+}
